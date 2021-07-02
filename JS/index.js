08f0856657a19b9dbd8541a72ddb595c58815b59
yoyo <yoyointheho@gmail.com>
2021-07-03 08:29:42 +0900

    Added scores to be shown with no limit.

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,8 @@
+let playerWin = 0;
+let computerWin = 0;
 
 //input your Name
-    function inputName() {
+function inputName() {
     alert('*phone is ringing.*');
     let firstAnswer = prompt('Answer the call? YES/NO');
     if (firstAnswer.toUpperCase() === 'YES') {
@@ -8,94 +10,89 @@
         document.getElementById('player-font').innerHTML =userName;
         return alert(`Stranger: "Lovely name ${userName}... Let's play a game of rock paper and scissors... By the way... I have locked you out in the phone booth..."`);
         }
-        else {
-            return alert('*phone keeps ringing');
-        }
+    else {
+        return alert('*phone keeps ringing');
     }
-//Pick random hand for the computer//
-    function computerPlay() {
+}
+
+//Pick random hand for the computer
+function computerPlay() {
     const hands = ['rock', 'paper', 'scissors'];
     const randomIndex = hands[Math.floor(Math.random() * hands.length)];
     return randomIndex;
-    } 
+} 
 console.log(computerPlay());
 
+//player clicked on a hand.
 function selectedPlayerHand(e) {
-    let playerScore = document.querySelector('.player-score');
-    let computerScore = document.querySelector('.compiter-score');
-    playRound(e);
-    console.log(e);
+    const result = playRound(e);
+    if (result === playerWin) {
+        return document.querySelector('.player-score').innerHTML = playerWin;
+    }
+    else if (result === computerWin) {
+        return document.querySelector('.computer-score').innerHTML = computerWin;
+    }
+    else return;
 }
-//play one round. compare the result. announce the result. user input automatically becomes case INsensetive//
+
+//play one round. compare the result. announce the result. user input automatically becomes case INsensetive
 function playRound(e) {
-    let winCondition;
     const computerSelection = computerPlay() ;
-    //user input = Rock//
+    //user input = Rock
     if (e === 'rock') {
         if (computerSelection === 'scissors') {
             console.log('you win. You:Rock Computer:Scissors');
-            winCondition = 'player';
-            return winCondition;
+            return playerWin += 1;
         }
         else if (computerSelection === 'paper') {
             console.log('you lost. You:Rock Computer:Paper');
-            winCondition = 'computer';
-            return winCondition;
+            return computerWin += 1;
         }
         else {
-            console.log('it is a tie. You:Rock Comouter:Rock');
-            winCondition = 'tie';
-            return winCondition;
+            return console.log('it is a tie. You:Rock Comouter:Rock');
+            
         }
     }
-    //user input = Paper//
+    //user input = Paper
     else if (e === 'paper') {
         if (computerSelection === 'rock') {
             console.log('You win. You:Paper Computer:Rock');
-            winCondition = 'player';
-            return winCondition;
+            return playerWin += 1;
         }
         else if (computerSelection === 'scissors') {
             console.log('you lost. You:Paper Computer:Scissors');
             winCondition = 'computer';
-            return winCondition;
+            return computerWin += 1;
         }
         else {
-            console.log('it is a tie. You:Paper Computer:Paper');         
-            winCondition = 'tie';
-            return winCondition;
+            return console.log('it is a tie. You:Paper Computer:Paper');         
+            
         }
     }
-    // user input = Scissors//
+    // user input = Scissors
     else if (e === 'scissors') {
         if (computerSelection === 'paper') {
             console.log('you win. You:Scissors Computer:Paper');
-            winCondition = 'player';
-            return winCondition;
+            return playerWin += 1;
         }
         else if (computerSelection === 'rock') {
             console.log('you lost. You:Scissors Computer:Rock');
-            winCondition = 'computer';
-
-            return winCondition;
+            return computerWin += 1;
         }
         else {
-            console.log('it\'s a tie. You:Paper Computer:Rock');
-            winCondition = 'tie';
-            return winCondition;
+            return console.log('it\'s a tie. You:Paper Computer:Rock');
         }
     }
 }
 
 
 //play 5 rounds(repeat playRound() and computerPlay()). keep the scores in console.log(). announce the winnder and loser at the end. //
-//Disabled for now to work on DOM.
-/*function game() {
+/*function game(e) {
     let playerScore = 0, computerScore = 0;
-    const userRoundInput = parseInt(prompt('how many rounds y\'all want?'));
-    console.log(userRoundInput);
-    for(i = 0; i < userRoundInput;  i++) {
-        let result = playRound();
+    //const userRoundInput = parseInt(prompt('how many rounds y\'all want?'));
+    //console.log(userRoundInput);
+    for(i = 0; i < 5;  i++) {
+        let result = playRound(e);
         if (result === 'player') {
             playerScore += 1;
             console.log('player score: ' + playerScore);
@@ -114,7 +111,7 @@ function playRound(e) {
     else {
         console.log('tied.')
     }
-}   */
+} */
 
 //inputName();
 
